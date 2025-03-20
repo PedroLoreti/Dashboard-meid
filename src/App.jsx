ca25c9624ad468464757463d0f514186c08bab1a
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2025-03-20 09:21:51 -0300

    Providers add

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,19 @@ function App() {
   return (
     <>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       <p>{JSON.stringify(pedidosList)}</p>
 =======
+=======
+>>>>>>> Stashed changes
       <ul>
         {pedidosList.map((pedido, index) => (
           <li key={index}>{JSON.stringify(pedido)}</li>
         ))}
       </ul>
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
     </>
   )
